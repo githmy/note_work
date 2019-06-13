b754a474782ade6f6e6717f3e04a71ab4c2bc9db
githmy <1272736235@qq.com>
2019-06-13 21:00:13 +0800

    v0.68

--- modules/genetic_storage/simu_env.py
+++ modules/genetic_storage/simu_env.py
@@ -20,6 +20,177 @@ class ShelfStatus(object):
         self.workertype = injson["workertype"]
 
 
+class SKUClass(object):
+    def __init__(self, injson):
+        self.name = injson["name"]
+        self.id = injson["id"]
+        self.shelf_type = injson["shelf_type"]
+        self.max_in_type = injson["max_in_type"]
+        self.weight = injson["weight"]
+        self.thresh_hold = injson["thresh_hold"]
+
+
+class SKUinfo(object):
+    def __init__(self, indata):
+        self.sku_list = []
+        self.init_skuinfo(indata)
+
+    def init_skuinfo(self, indata):
+        for i1 in indata:
+            self.sku_list.append(SKUClass(i1))
+
+    def insert_sku(self, single_sku_info_json):
+        self.sku_list.append(SKUClass(single_sku_info_json))
+
+    def update_sku(self, single_sku_info_json):
+        for i1 in self.sku_list:
+            if i1.id == single_sku_info_json["id"]:
+                i1 = SKUClass(single_sku_info_json)
+                return 0
+
+    def delete_sku(self, single_sku_info_json):
+        for i1 in self.sku_list:
+            if i1.id == single_sku_info_json["id"]:
+                self.sku_list.remove(i1)
+                return 0
+
+
+class SimuBench(object):
+    def __init__(self, env_conf, effi):
+        self._conf = env_conf
+        self.bench_shift_amount = self._conf["bench_shift_amount"] * effi
+        self.bench_shift_class = self._conf["bench_shift_class"] * effi
+        self.bench_shift_order = self._conf["bench_shift_order"] * effi
+        # 统计计时
+        self.time_accum = 0.0
+        # 该任务欠计时
+        self.time4finish_mission = 0.0
+        # 该任务波次信息
+        self.batch_mission = []
+        # 该任务耗时
+        self.consume_mission = 0.0
+        # 该分拣台波次队列
+        self.batch_waiting = []
+        # 货架id，剩余时间
+        self.onway_time = {}
+
+    # 波次订单处理时间
+    def deal_time(self):
+        classnum = 0
+        amountnum = 0
+        shelfnum = 0
+        classlist = []
+        shelflist = []
+        for i1 in self.batch_mission:
+            amountnum += i1.getnum
+            if i1.cellid not in classlist:
+                classlist += 1
+            if i1.shelfid not in shelflist:
+                shelfnum += 1
+        consume_mission = amountnum * self.bench_shift_amount + classnum * self.bench_shift_class + shelfnum * self.bench_shift_order
+        self.consume_mission = consume_mission
+        self.time4finish_mission = consume_mission
+        return consume_mission
+
+    def onway_time(self):
+        # 货架id，剩余时间
+        self.onway_time = {}
+
+    # 任务结束操作
+    def mission_finish(self):
+        self.batch_mission = []
+        self.consume_mission = 0.0
+        self.time4finish_mission = 0.0
+        # 检查该分拣台的队列，读入新任务。
+        if 0 != len(self.batch_waiting):
+            tmpid = self.batch_waiting[0].batchid
+            self.batch_mission = [i2 for i2 in self.batch_waiting if i2.batchid == tmpid]
+            self.batch_waiting = [i2 for i2 in self.batch_waiting if i2.batchid != tmpid]
+            # 清空重置原有任务完成时间
+            self.deal_time()
+            self.time4finish_mission = self.consume_mission
+        # 设置刚执行完的货架是否需要补货
+        return
+
+
+class SimuReplenish(object):
+    def __init__(self, env_conf, effi):
+        self._conf = env_conf
+        self.replenish_shift_amount = self._conf["replenish_shift_amount"] * effi
+        self.replenish_shift_class = self._conf["replenish_shift_class"] * effi
+        self.replenish_shift_shelf = self._conf["replenish_shift_shelf"] * effi
+        # 统计计时
+        self.time_accum = 0.0
+        # # 该任务欠计时
+        self.time4finish_mission = 0.0
+        # 该任务补货信息
+        self.batch_mission = []
+        # 该任务耗时
+        self.consume_mission = 0.0
+        # 该补货台货架队列
+        self.batch_waiting = []
+        # 货架id，剩余时间
+        self.onway_time = {}
+
+    # 补货处理时间
+    def deal_time(self):
+        classnum = 0
+        amountnum = 0
+        shelfnum = 0
+        classlist = []
+        shelflist = []
+        for i1 in self.batch_mission:
+            amountnum += i1.shortcommodity
+            if i1.cellid not in classlist:
+                classlist += 1
+            if i1.shelfid not in shelflist:
+                shelfnum += 1
+        consume_mission = amountnum * self.replenish_shift_amount + classnum * self.replenish_shift_class + shelfnum * self.replenish_shift_shelf
+        self.consume_mission = consume_mission
+        self.time4finish_mission = consume_mission
+        return consume_mission
+
+    def onway_time(self):
+        # 货架id，剩余时间
+        self.onway_time = {}
+
+    # 任务结束操作
+    def mission_finish(self):
+        self.batch_mission = []
+        self.consume_mission = 0.0
+        self.time4finish_mission = 0.0
+        # 检查该分拣台的队列，读入新任务。
+        if 0 != len(self.batch_waiting):
+            tmpid = self.batch_waiting[0].batchid
+            self.batch_mission = [i2 for i2 in self.batch_waiting if i2.batchid == tmpid]
+            self.batch_waiting = [i2 for i2 in self.batch_waiting if i2.batchid != tmpid]
+            # 清空重置原有任务完成时间
+            self.deal_time()
+            self.time4finish_mission = self.consume_mission
+        # 设置刚执行完的货架是取消补货标记
+        return
+
+
+class SimuBatchOrder(object):
+    def __init__(self, injson):
+        self.batchid = injson["batchid"]
+        self.orderid = injson["orderid"]
+        self.workbetchid = injson["workbetchid"]
+        self.shelfid = injson["shelfid"]
+        self.cellid = injson["cellid"]
+        self.commodityid = injson["commodityid"]
+        self.getnum = injson["getnum"]
+
+
+class ShelfStatus(object):
+    def __init__(self, injson):
+        self.shelfid = injson["shelfid"]
+        self.shelftypeid = injson["shelftypeid"]
+        self.shelfstatus = injson["shelfstatus"]
+        self.workerid = injson["workerid"]
+        self.workertype = injson["workertype"]
+
+
 class ShelfInfo(object):
     def __init__(self, injson):
         self.shelfid = injson["shelfid"]
@@ -46,21 +217,6 @@ class ShelfType(object):
         self.maxcell = injson["maxcell"]
 
 
-class OrderInfo(object):
-    def __init__(self, injson):
-        self.orderid = injson["orderid"]
-        self.commodityid = injson["urgent"]
-        self.commodityid = injson["indate"]
-        self.num = injson["num"]
-
-
-class OrderDetail(object):
-    def __init__(self, injson):
-        self.orderid = injson["orderid"]
-        self.commodityid = injson["commodityid"]
-        self.num = injson["num"]
-
-
 class SimuEnv(object):
     def __init__(self, conf, model_json):
         self._conf = conf
@@ -84,15 +240,25 @@ class SimuEnv(object):
         self._virtual_current_shelf = 0
         # 虚拟工作台状态初始化
         self._virtual_bench = {}
-        # self._virtual_bench_init()
+        self._virtual_bench_init()
         # 虚拟补货台状态初始化
         self._virtual_replenish = {}
-        # self._virtual_replenish_init()
+        self._virtual_replenish_init()
 
     # 初始化货架
     def _shelf_init(self, shelfjson):
         for i2 in shelfjson:
             self.shelf_list.append(ShelfInfo(i2))
+
+    # 初始化分拣台
+    def _virtual_bench_init(self):
+        for i in range(self.bench_maxnum):
+            self._virtual_bench[str(i)] = SimuBench(self._conf, math.cos(i * 0.06))
+
+    # 初始化补货信息。
+    def _virtual_replenish_init(self):
+        for i in range(self.replenish_maxnum):
+            self._virtual_replenish[str(i)] = SimuReplenish(self._conf, math.cos(i * 0.04))
 
     # 订单引擎
     def _order_generater(self):
@@ -114,6 +280,17 @@ class SimuEnv(object):
     def _get_batch_doing_length(self):
         return sum([1 for i1 in self._virtual_bench if
                     0 != len(self._virtual_bench[i1].batch_mission)]) / self._betch_use_num
+
+    # 尝试加入波次任务队列，自动调用分配模块。
+    def _try_add_batch(self, order_batchs):
+        lenth = self._get_batch_length()
+        if lenth > self._betch_qeueu_up:
+            self._betch_use_num += 1
+        elif lenth < self._betch_qeueu_down:
+            if lenth > 1:
+                self._betch_use_num -= 1
+        for i1 in order_batchs:
+            self._virtual_bench[i1].batch_waiting.append(SimuBatchOrder(i1))
 
     def _get_replenish_length(self):
         return sum([len(self._virtual_replenish[i1].batch_waiting) for i1 in self._virtual_replenish if
@@ -164,13 +341,13 @@ class SimuEnv(object):
                                                                  if i2.batchid != tmpid]
 
     # 策略(流程规则，环境互动 - 订单缓存表的动态变化，)
-    def strategy_flow(self, indata, simustrategy, start=None, to=None, threadid=1):
+    def strategy_flow(self, indata, simustratey, start=None, to=None, threadid=1):
         # 订单信息，取消订单信息，货架货物表
-        self._read_interval = simustrategy._read_interval
-        self._betch_qeueu_down = simustrategy._betch_qeueu_down
-        self._betch_qeueu_up = simustrategy._betch_qeueu_up
-        self._replenish_qeueu_down = simustrategy._replenish_qeueu_down
-        self._replenish_qeueu_up = simustrategy._replenish_qeueu_up
+        self._read_interval = simustratey._read_interval
+        self._betch_qeueu_down = simustratey._betch_qeueu_down
+        self._betch_qeueu_up = simustratey._betch_qeueu_up
+        self._replenish_qeueu_down = simustratey._replenish_qeueu_down
+        self._replenish_qeueu_up = simustratey._replenish_qeueu_up
         self._betch_use_num = 1
         self._replenish_use_num = 1
         # 数据转化
@@ -212,16 +389,16 @@ class SimuEnv(object):
                 # self._cancel_order_simu(time_range)
                 time_start = time.time()
                 # 输入self._virtual_calcu_order，货架信息已经扣除了占用量，返回波次订单信息，随机撤销订单
-                simustrategy.pick_replenish_strategy()
-                order_batchs, batchbetch_all, replenish_lists = simustrategy.strategy_policy(self._virtual_calcu_order,
-                                                                                            self._virtual_current_shelf,
-                                                                                            self._betch_use_num, )
+                order_batchs, replenish_lists = simustratey.strategy_policy(self._virtual_calcu_order,
+                                                                            self._virtual_current_shelf,
+                                                                            self._betch_use_num,
+                                                                            self._replenish_use_num)
                 self._calc_time_delay = time.time() - time_start
                 if self._calc_time_delay < self._read_interval:
                     self._timecalu_left = self._read_interval
                 else:
                     self._timecalu_left = self._calc_time_delay
-                # self._try_add_batch(order_batchs)
+                self._try_add_batch(order_batchs)
                 self._try_add_replenish(replenish_lists)
                 self._add2batch()
                 self._add2replenish()
@@ -234,22 +411,22 @@ class SimuEnv(object):
                 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(finishtime))))
                 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(self._virtual_current_stamp))))
                 print(self._get_batch_length(), self._get_batch_doing_length())
-                simustrategy._betch_qeueu_down = self._betch_qeueu_down
-                simustrategy._betch_qeueu_up = self._betch_qeueu_up
-                simustrategy._replenish_qeueu_down = self._replenish_qeueu_down
-                simustrategy._replenish_qeueu_up = self._replenish_qeueu_up
+                simustratey._betch_qeueu_down = self._betch_qeueu_down
+                simustratey._betch_qeueu_up = self._betch_qeueu_up
+                simustratey._replenish_qeueu_down = self._replenish_qeueu_down
+                simustratey._replenish_qeueu_up = self._replenish_qeueu_up
                 print("统计各种信息：")
                 # 多机版要改到数据库
                 print("finishing！")
-                simustrategy.dump_paras(
-                    os.path.join(simustrategy._para_path, "gene_%s_%s.json" % (threadid, self.cost_fun())))
+                simustratey.dump_paras(
+                    os.path.join(simustratey._para_path, "gene_%s_%s.json" % (threadid, self.cost_fun())))
                 return 0
 
     # 策略更新(流程规则，环境互动 - 订单缓存表的动态变化，)
-    def strategy_update(self, indata, simustrategy, start=None, to=None):
+    def strategy_update(self, indata, simustratey, start=None, to=None):
         # 变更时只考虑未占用货架，且设置货架状态待变更3
         # 订单历史信息表（带时间参数，取之后左右的包含新单未处理的），货架类型表，商品信息表，储位sku可容纳数量映射表
-        simustrategy.strategy_update()
+        simustratey.strategy_update()
         pass
 
     # 订单取消(随机模拟，正在执行的忽略，分好波次的删除，否则动态删除缓存)
